ce9042a87489e7af5188dcab816daadc43ff4a7b
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2023-07-07 17:51:11 +0200

    yaml config

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,65 @@
+use std::collections::HashMap;
 use std::fs;
 use std::io::{self, Read};
-use clap::{App, Arg};
+use serde::{Deserialize, Serialize};
 
-use jg::generator;
-use jg::parser::{self, Rule};
-use pest::Parser;
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag="type")]
+enum Model {
+    Ref {
+        model: String
+    },
+    Object {
+        properties: Option<std::collections::BTreeMap<String, Model>>,
+        value: Option<serde_yaml::Mapping>
+    },
+    Array {
+        items: Option<Vec<Model>>,
+        value: Option<serde_yaml::Sequence>
+    },
+    String {
+        value: Option<String>
+    },
+    Integer {
+        value: Option<i64>,
+        min: Option<i64>,
+        max: Option<i64>,
+    },
+    Float {
+        value: Option<f64>,
+        min: Option<f64>,
+        max: Option<f64>,
+    },
+    Timestamp {
+        format: Option<String>
+    }
+}
 
 fn main() {
-    // Parse command line arguments using clap
-    let  matches = App::new("JSON Generator")
-        .arg(
-            Arg::with_name("input")
-                .value_name("INPUT")
-                .help("Input file path")
-                .required(false),
-        )
-        .get_matches();
+    let mut file = fs::File::open("config.yaml").expect("Failed to open input file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Failed to read input file");
 
-    // Read the input file or stdin
-    let json_schema = match matches.value_of("input") {
-        Some(input_path) => {
-            let mut file = fs::File::open(input_path).expect("Failed to open input file");
-            let mut contents = String::new();
-            file.read_to_string(&mut contents)
-                .expect("Failed to read input file");
-            contents
-        }
-        None => {
-            // let mut input = String::new();
-            // io::stdin().read_to_string(&mut input).expect("Failed to read from stdin");
-            // input
-            "{ \"myKey\": string(5,10, \"asd\", \"qwe\") }".to_string()
-            //"{\"myKey\": [123.321, string(5,10, \"asd\", \"qwe\")]}".to_string()
-        }
-    };
+    let config: serde_yaml::Value = serde_yaml::from_str(&contents).unwrap();    
+    let data = &config["data"];
+    let schemas = &config["components"]["schemas"];
+    let constants = &config["components"]["constants"];
+    let sequences = &config["components"]["sequences"];
 
-    // // Generate the JSON object
-    // match generator::generate_json(json_schema) {
-    //     Ok(json) => println!("{}", json),
-    //     Err(err) => eprintln!("Error: {}", err),
-    // }
-
-     // Parse JSON generation definitions
-     let parsed_json = match parser::JsonParser::parse(Rule::json, &json_schema) {
-        Ok(mut pairs) => {
-            let pair = pairs.next().unwrap();
-            parser::parse_value(pair)
-        }
-        Err(err) => {
-            eprintln!("Error parsing JSON generation definitions: {}", err);
-            return;
-        }
-    };
+    println!("{:?}", data);
+    println!();
+    let d : Model = serde_yaml::from_value(data.clone()).unwrap();
+    println!("{:?}", d);
+    
+    println!();
+    println!();
+    println!("{}", serde_yaml::to_string(&d).unwrap());
 
 
-    // Generate JSON
-    let generated_json = generator::generate_json(&parsed_json);
+    println!();
+    println!();
+    println!("{}", serde_json::to_string_pretty(&d).unwrap());
 
-    // Print generated JSON
-    println!("{}", serde_json::to_string_pretty(&generated_json).unwrap());
-
+    //println!("{}", serde_json::to_string_pretty(schemas).unwrap());
 }
